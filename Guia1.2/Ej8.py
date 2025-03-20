dadb1d383ec286eec04bef00b55f63872e28f184
Alejo Lang <alejolang123@gmail.com>
2025-03-20 00:34:50 -0300

    Add another way of doing the exercise

--- Guia1.2/Ej8.py
+++ Guia1.2/Ej8.py
@@ -1,6 +1,7 @@
 import math
 N = int(input("Introduce un número entero N para convertirlo a binario: "))
 binario = ""
+binarioConFunc = format(N, 'b')
 
 while N > 1:
   binario = str(N%2) + binario
@@ -8,3 +9,4 @@ while N > 1:
 if N == 1:
   binario = "1" + binario
 print(binario)
+print(binarioConFunc)
